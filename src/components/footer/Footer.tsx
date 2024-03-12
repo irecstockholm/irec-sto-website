7a765391b5404bebade5c3f36d33effa438cbb8b
Ela Yudhanira <elayudhanira@gmail.com>
2024-03-12 17:07:32 +0100

    feat: #3 add header and footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,57 @@
+import cn from 'classnames';
+import HeaderLogo from '../Header/Logo';
 import styles from './Footer.module.scss';
 
 const Footer = () => {
   return <div className={styles.footer}>
-    Footer
+    <div className={cn(styles.footerCol, styles.footerLogo)}>
+        <HeaderLogo />
+        <div className={styles.footerLogoText}>
+          <h2>IREC Stockholm</h2>
+          <h3>International Reformed <br/>Evangelical Church</h3>
+        </div>
+    </div>
+
+    <div className={styles.footerCol}>
+      <h3>Contact</h3>
+      <p>+46 72 593 98 38</p>
+      <p>irec.stockholm@gmail.com</p>
+    </div>
+
+    <div className={styles.footerCol}>
+      <h3>Address</h3>
+      <p>IREC Stockholm</p>
+      <p><a href='https://maps.app.goo.gl/92L6XevnhJQAqp7YA' rel='noreferrer' target='_blank'>Kristet Center, Hagalundsgatan 9</a></p>
+      <p>Solna, Stockholm</p>
+    </div>
+
+    <div className={styles.footerCol}>
+      <ul>
+        <li><a href='https://www.instagram.com/irec.europe/' target='_blank' rel='noreferrer'>Instagram</a></li>
+        <li><a href='https://www.facebook.com/people/PRII-Stockholm-IREC-Stockholm/100089208450489/?locale2=en' target='_blank' rel='noreferrer'>Facebook</a></li>
+        <li><a href='https://www.youtube.com/channel/UCJGpw2y2fAQ97CMXRpdPGMw' target='_blank' rel='noreferrer'>Youtube</a></li>
+      </ul>
+    </div>
+
+    <div className={styles.footerCol}>
+      <ul>
+        <li><a href='https://irec-berlin.org/' target='_blank' rel='noreferrer'>IREC Berlin</a></li>
+        <li><a href='http://www.grii-bern.org/en/' target='_blank' rel='noreferrer'>IREC Bern</a></li>
+        <li><a href='https://www.grii-hamburg.org/en-gb' target='_blank' rel='noreferrer'>IREC Hamburg</a></li>
+        <li><a href='http://translate.google.com/translate?js=n&sl=id&tl=en&u=http://grii-munich.org' target='_blank' rel='noreferrer'>IREC Munich</a></li>
+      </ul>
+    </div>
+
+    <div className={styles.footerCol}>
+      <ul>
+        <li><a href='https://stemi.org/' target='_blank' rel='noreferrer'>Stephen Tong Evangelical Ministry International</a></li>
+        <li><a href='https://www.recjakarta.com/' target='_blank' rel='noreferrer'>Reformed Evangelical Church Jakarta</a></li>
+        <li><a href='https://www.reformed21.tv/' target='_blank' rel='noreferrer'>Reformed 21</a></li>
+        <li><a href='https://plkmomentum.com/' target='_blank' rel='noreferrer'>Momentum Christian Literature</a></li>
+        <li><a href='https://front.aulasimfoniajakarta.com/' target='_blank' rel='noreferrer'>Aula Simfonia Jakarta</a></li>
+      </ul>
+    </div>
+
   </div>;
 }
 
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,13 @@
+import { Link, NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import cn from 'classnames';
 import HeaderLogo from './Logo';
 import styles from './Header.module.scss';
-import { Link } from 'react-router-dom';
 
 const Header = () => {
+
+  const [isNavShown, setIsNavShown] = useState(false);
+
   return <header className={styles.header}>
     <div className={styles.headerLogo}>
       <Link to='/' className={styles.headerLink}>
@@ -10,10 +15,14 @@ const Header = () => {
         <h1 className={styles.headerTitle}>IREC Stockholm</h1>
       </Link>
     </div>
-    <nav className={styles.nav}>
-      <Link to='/'>Home</Link>
-      <Link to='/events'>News &amp; Events</Link>
-      <Link to='/'>Our Church</Link>
+    <div className={styles.menuButtonWrapper} onClick={() => setIsNavShown(true)}>
+      <button className={styles.menuButton}></button>
+    </div>
+    <nav className={cn(styles.nav, isNavShown ? styles.isShown : '')}>
+      <div className={styles.closeNav} onClick={() => setIsNavShown(false)}>Close</div>
+      <NavLink to='/' className={({isActive}) => isActive ? styles.active : ''}>Home</NavLink>
+      <NavLink to='/events' className={({isActive}) => isActive ? styles.active : ''}>News &amp; Events</NavLink>
+      <NavLink to='/church' className={({isActive}) => isActive ? styles.active : ''}>Our Church</NavLink>
     </nav>
   </header>;
 }
