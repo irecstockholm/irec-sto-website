ef669a35f713c7c7ca10216a5435d4b192ffa23c
Ela Yudhanira <elayudhanira@gmail.com>
2024-03-12 10:37:24 +0100

    feat: initial structure

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main className="main">
+        Main page
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
